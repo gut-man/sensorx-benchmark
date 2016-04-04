5d2995024fe3d611a6d7161f5e6922b0647c11b2
Manuel Guttenberger <ln-git@mgu.io>
2016-04-04 20:54:53 +0200

    timer removed

--- mqtt_client/mqtt.js
+++ mqtt_client/mqtt.js
@@ -1,14 +1,12 @@
 var mqtt    = require('mqtt');
 
 var lan = 1;
-var i = 0;
 
 if(lan == 1)
 	var client  = mqtt.connect('mqtt://127.0.0.1');
 else
 	var client  = mqtt.connect('mqtt://test.mosquitto.org');
-var t_star  = "";
-var t_stop ="";
+
 client.on('connect', function () {
   client.subscribe('bench/arduino');
   client.subscribe('bench/esp8266');
@@ -16,8 +14,7 @@ client.on('connect', function () {
 });
 
 client.on('message', function (topic, message) {
-  i++;
-  console.log(i+";"+Date.now() +";"+message);
+  console.log(Date.now() +";"+message);
 });
 
 
